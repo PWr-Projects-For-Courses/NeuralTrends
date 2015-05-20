91ad0678a04d08cf21edbfddaa37d2f48c57cbe4
Jacek Miszczak <jacekmiszczak@gmail.com>
2015-05-20 21:00:39 +0200

    Small test enhancement

--- src/test/groovy/com/github/fm_jm/neuraltrends/data/NormalizerTest.groovy
+++ src/test/groovy/com/github/fm_jm/neuraltrends/data/NormalizerTest.groovy
@@ -6,6 +6,9 @@ class NormalizerTest extends GroovyTestCase {
         assert Normalizer.normalize(0.6, 0.0, 2.0) == 0.3
         assert Normalizer.normalize(0.0, 0.0, 2.0) == 0.0
         assert Normalizer.normalize(2.0, 0.0, 2.0) == 1.0
+        assert Normalizer.normalize(0.0, -1.0, 1.0) == 0.5
+        assert Normalizer.normalize(1.0, -1.0, 1.0) == 1.0
+        assert Normalizer.normalize(-1.0, -1.0, 1.0) == 0.0
         def caught
         try {
             Normalizer.normalize(-1.0, 0.0, 2.0)
